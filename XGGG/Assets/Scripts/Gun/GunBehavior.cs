444469e1f5f78adf1f0d20969f188a760392cf62
Will Saymon <wsaymonoficial@gmail.com>
2019-04-30 17:28:00 -0300

    [Feature01] Done
    Add Wyru Scene with a simple config. Planet Prefab was created with gun logic. Input Manager get player input and call a method from GunBehavior

---
+++ XGGG/Assets/Scripts/Gun/GunBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehavior : MonoBehaviour
+{
+
+    enum GunState
+    {
+        idle,
+        movingLeft,
+        movingRight
+    }
+
+    [SerializeField][Range(0,10)]
+    float rotateSpeed = 0;
+    Transform planetTransform;
+    GunState state;
+
+
+    void Start()
+    {
+        planetTransform = transform.parent;
+        state = GunState.idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state != GunState.idle){
+            if(state == GunState.movingLeft){
+                Rotate(rotateSpeed);
+            }
+
+            if(state == GunState.movingRight){
+                Rotate(-rotateSpeed);
+            }
+        }
+    }
+
+    void Rotate(float speed){
+        transform.RotateAround(planetTransform.transform.position, new Vector3(0,0,1), speed);
+    }
+
+    public void MoveToLeft(){
+        state = GunState.movingLeft;
+    }
+
+    public void MoveToRight(){
+        state = GunState.movingRight;
+    }
+
+    public void Stop(){
+        state = GunState.idle;
+    }
+
+}
---
+++ XGGG/Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GunBehavior currentGun;
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0.1)
+        {
+            currentGun.MoveToRight();
+        }
+        else if (Input.GetAxis("Horizontal") < -0.1)
+        {
+            currentGun.MoveToLeft();
+        }
+        else
+        {
+            currentGun.Stop();
+        }
+    }
+}
