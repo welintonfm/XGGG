783d37da7e8000352d82cabaa19645eb45deb58e
Will Saymon <wsaymonoficial@gmail.com>
2019-05-01 19:42:52 -0300

    Created leaderboard gui, save score system

---
+++ XGGG/Assets/Scripts/GUI/Leaderboard/LeaderboardGUIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class LeaderboardGUIController : MonoBehaviour
+{
+    [Header("Leaderboard")]
+    public TextMeshProUGUI[] names;
+    public TextMeshProUGUI[] numbers;
+
+    // [Header("Submit Score System")]
+
+    private void OnEnable(){
+        UpdateLeaderboard();
+    }
+
+    void UpdateLeaderboard(){
+        List<Score> scores = Leaderboard.Scores;
+        for (int i = 0; i < names.Length; i++)
+        {
+            if(i < scores.Count){
+                names[i].SetText(scores[i].name);
+                numbers[i].SetText(scores[i].value.ToString());
+            }
+            else{
+                names[i].SetText("---");
+                numbers[i].SetText("0");
+            }
+        }
+    }
+}
---
+++ XGGG/Assets/Scripts/Leaderboard/Leaderboard.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class Leaderboard
+{
+    static List<Score> scores;
+
+    public static List<Score> Scores{
+        get{
+            if (scores == null)
+            {
+                scores = Load();
+            }
+            return scores;
+        }
+    }
+    public static void AddScore(string name, int value)
+    {
+        if (scores == null)
+        {
+            scores = Load();
+        }
+
+        scores.Add(new Score(name, value));
+
+        scores = scores.OrderBy(x => x.value).Reverse().Take(10).ToList();
+
+        Save();
+    }
+
+    public static void Save()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/save.xggg";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        ScoresData data = new ScoresData(scores.ToArray());
+        formatter.Serialize(stream, data);
+        stream.Close();
+
+    }
+
+    public static List<Score> Load()
+    {
+        string path = Application.persistentDataPath + "/save.xggg";
+        if (File.Exists(path))
+        {
+            Debug.Log("File Exist");
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            ScoresData data = formatter.Deserialize(stream) as ScoresData;
+            stream.Close();
+            return new List<Score>(data.data);
+        }
+
+        return new List<Score>();
+    }
+
+
+}
+
+[System.Serializable]
+public class Score
+{
+    public string name;
+    public int value;
+
+    public Score(string name, int value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+}
+
+[System.Serializable]
+public class ScoresData
+{
+    public Score[] data;
+
+    public ScoresData(Score[] data)
+    {
+        this.data = data;
+    }
+}
