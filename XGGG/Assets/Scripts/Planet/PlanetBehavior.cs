b5bb77920f1ae2ab803f8a30f61d986db2e1b11b
Will Saymon <wsaymonoficial@gmail.com>
2019-05-01 13:42:34 -0300

    Orbit path and planet orbitation done

--- XGGG/Assets/Scripts/Planet/PlanetBehavior.cs
+++ XGGG/Assets/Scripts/Planet/PlanetBehavior.cs
@@ -6,20 +6,41 @@ public class PlanetBehavior : MonoBehaviour
 {
     public GameObject selectIndicator;
 
+    public Ellipse orbit;
+
+    [Range(0f, 1f)]
+    public float orbitProgress;
+    public float orbitPeriod = 3;
+
+
+    float cTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cTime = orbitProgress*orbitPeriod;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePosition();
     }
 
 
-    public void Select(bool b){
+    public void Select(bool b)
+    {
         selectIndicator.SetActive(b);
+    }
+
+
+    void UpdatePosition()
+    {
+        if(orbit == null) return;
+
+        float progress = (cTime / orbitPeriod)%1f;
+        
+        transform.localPosition = orbit.Evaluate(progress);
+        cTime += Time.deltaTime;
     }
 }
