58ceaa9614b6f212d188a711a3fec3244f4bf779
Will Saymon <wsaymonoficial@gmail.com>
2019-05-04 00:09:58 -0300

    Added Main menu + scenes transition

---
+++ XGGG/Assets/Scripts/GUI/Menu/MenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MenuButton : MonoBehaviour
+{
+    public AudioClip submitSound;
+    public UnityEvent OnPressed;
+    public void Action()
+    {
+        if(OnPressed != null){
+            OnPressed.Invoke();
+        }
+    }
+}
