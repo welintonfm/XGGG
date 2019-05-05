d5a293e99f56923f7b9fc5bfddeddfede39af3e8
Welinton Faria <welintonfm@gmail.com>
2019-05-05 15:34:37 -0300

    [UPDATE] Sonzinho das naves

---
+++ XGGG/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioManager audioManager;
+    public void Play(string clipname)
+    {
+        if(audioManager== null)
+        {
+            audioManager = FindObjectOfType<AudioManager>();
+        }
+        
+        audioManager.PlaySound(clipname);
+        Debug.Log("tocou sonzinho:  teixe, morri");
+        return;
+    }
+}
